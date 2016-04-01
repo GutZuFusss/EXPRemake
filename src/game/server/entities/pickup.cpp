fd84d1e07681ac396d1ea2689cfddeec1cf586d1
xushTW <g8730443@trbvm.com>
2016-04-01 10:54:40 +0200

    removed debugging stuff

--- src/game/server/entities/pickup.cpp
+++ src/game/server/entities/pickup.cpp
@@ -291,9 +291,6 @@ void CPickup::CreateRandomFromBot(int lvl, vec2 Pos)
 				m_Subtype = WEAPON_GRENADE; //4%
 			}
 		}
-
-		m_Type = POWERUP_WEAPON;
-		m_Subtype = WEAPON_FREEZER;
 	}
 	else if(lvl == 2)
 	{
